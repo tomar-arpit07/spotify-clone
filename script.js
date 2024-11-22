02b4eb8dfa4c412d52260cb84f102abefd505137
tomar_arpit07 <tomararpit187@gmail.com>
2024-11-22 23:07:56 +0530

    again updated api

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`https://github.com/tomar-arpit07/spotify-clone/tree/main/${folder}/`);
+    let a = await fetch(`./${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -69,7 +69,7 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`https://github.com/tomar-arpit07/spotify-clone/tree/main/songs/`);
+    let a = await fetch(`./songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -81,7 +81,7 @@ async function displayAlbums() {
 
         if (e.href.includes("/songs") && !e.href.includes(".htaccess")){
             let folder = e.href.split("/").slice("-2")[0]
-            let a = await fetch(`https://github.com/tomar-arpit07/spotify-clone/tree/main/songs/${folder}/info.json`);
+            let a = await fetch(`./songs/${folder}/info.json`);
             let response = await a.json();
             cardContainer.innerHTML = cardContainer.innerHTML +
                 `<div data-folder="${folder}" class="card">
